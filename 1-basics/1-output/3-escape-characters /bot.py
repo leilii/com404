16504bd0e4fc06369d1a18efbed15eba06158247
leilii <43784088+leilii@users.noreply.github.com>
2018-10-02 14:58:59 +0100

    Create 3-escape-characters /bot.py
    Added code to display escape characters.

---
+++ 1-basics/1-output/3-escape-characters /bot.py
@@ -0,0 +1,11 @@
+# Display a simple message
+print("System Failure Imminent!")
+print("System rboot has been initiated")
+print("")
+print("")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting  hover engine")
+print("")
+print("")
+print("System online.")
