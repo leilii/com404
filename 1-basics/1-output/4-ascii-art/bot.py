518829919fa717fb1f3b7fa309f01b67f8859267
leilii <43784088+leilii@users.noreply.github.com>
2018-10-02 15:27:29 +0100

    Create 4-ascii-art/bot.py
    Added code to display an ascii art robot.

---
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -0,0 +1,12 @@
+# Display an asci robot
+
+print("\t ########")
+print("\t # O  O #")
+print("\t #  _   #")
+print("\t ########")
+print("\t    ##")
+print("\t ########")
+print("\t #      # ")
+print("\t #      # ")
+print("\t #      # ")
+print("\t ######## ")
