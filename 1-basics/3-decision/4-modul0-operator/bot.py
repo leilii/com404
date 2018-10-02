c6dd0fe2b84ff6a395bb03fb5b723a6ba1c13c53
leilii <43784088+leilii@users.noreply.github.com>
2018-10-02 16:55:14 +0100

    Create 4-modul0-operator/bot.py
    Added code to demonstrate use of modulo operator

---
+++ 1-basics/3-decision/4-modul0-operator/bot.py
@@ -0,0 +1,8 @@
+#Read whole number UserWarning.
+#work out if the number is even.
+print("Please enter a number.")
+wholenumber=int(input())
+if (wholenumber%2 == 0):
+    print("The number is even")
+else:
+  print("The number is odd")
