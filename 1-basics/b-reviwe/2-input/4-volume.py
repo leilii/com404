d212edc86cd6998e77942b3a0b302561479a036c
leilii <43784088+leilii@users.noreply.github.com>
2018-10-09 15:26:29 +0100

    Create 4-volume.py
    calculate volum

---
+++ 1-basics/b-reviwe/2-input/4-volume.py
@@ -0,0 +1,19 @@
+import math
+#ask user to enter a shape
+print("Please enter a shap cy for cylander or co for cone")
+#read user respond
+shape=input()
+#ask user for radius
+print("Please enter a Radius")
+Radius=float(input())
+#ask user for hight
+print("Please enter a hight")
+Hight=float(input())
+
+V1 = math.pi *(pow(radius, 2))*Hight
+#decide if the sape is cylander
+if (shape=="cy"):
+   print("The volum of the cylander with" + str(Radius)+"and hight"+Hight +"is"+round(V1,2))
+elif(shap=="co"): 
+   V2=V1/3
+  print("The volum of the cone with" + str(Radius)+"and hight"+Hight "is"+round(V2,2))
