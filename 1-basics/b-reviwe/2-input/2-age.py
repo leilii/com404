705b68e100f46b391b968856f51cb2442902113d
leilii <43784088+leilii@users.noreply.github.com>
2018-10-09 14:42:12 +0100

    Create 2-age.py
    calculate the age in days

---
+++ 1-basics/b-reviwe/2-input/2-age.py
@@ -0,0 +1,10 @@
+import math
+#ask user to enter age in years
+print("Please enter age in years")
+#read user respond
+Days_In_Year=365.25
+age=int(input())
+#calculate the area and circumference
+days=(age *Days_In_Year)
+#print the result
+print("The age of person in days are:",days)
