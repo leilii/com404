89b103e1d79e25e0295e29b186cb450d96185f38
leilii <43784088+leilii@users.noreply.github.com>
2018-10-09 16:23:43 +0100

    Create 4-repetition/while.py
    demonstrat while

---
+++ 1-basics/b-reviwe/2-input/4-repetition/while.py
@@ -0,0 +1,10 @@
+#ask user how many robots to print
+print("How many asci robots should I print")
+num_robot=int(input())
+count=0
+#display robots
+while (count>num_robot):
+  print("#####")
+  print("#    #")
+  print("#####")
+count=count+1
