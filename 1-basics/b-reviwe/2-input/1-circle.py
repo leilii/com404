375abf5794ccb6e80245b256743989603fab997f
leilii <43784088+leilii@users.noreply.github.com>
2018-10-09 14:40:57 +0100

    Create b-reviwe/2-input/1-circle.py

---
+++ 1-basics/b-reviwe/2-input/1-circle.py
@@ -0,0 +1,10 @@
+import math
+#ask user to enter radius
+print("Please enter radius")
+#read user respond
+radius=float(input())
+#calculate the area and circumference
+area=math.pi *(radius *radius)
+#alternativly:
+#area=math.pi *(radius ** 2)
+#area=math.pi * pow(radius,2)
