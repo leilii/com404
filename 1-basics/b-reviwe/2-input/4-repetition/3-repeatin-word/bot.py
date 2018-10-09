d799ddda0b867443b9069510607f5ef237020616
leilii <43784088+leilii@users.noreply.github.com>
2018-10-09 16:31:49 +0100

    Create 3-repeatin-word/bot.py
    Added code to demonstrate how the use of the function with loop

---
+++ 1-basics/b-reviwe/2-input/4-repetition/3-repeatin-word/bot.py
@@ -0,0 +1,9 @@
+#ask userfor a word
+print("Please enter a word")
+#read user respond
+word=input()
+#work lenght of word
+len_word=(len(word))
+#display word
+for l in range(0,len_word,1):
+    print(word)
