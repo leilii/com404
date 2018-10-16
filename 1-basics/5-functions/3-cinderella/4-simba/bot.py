f7e99d191c9035c96344f76b9e0fbf5e40c2eac8
leilii <43784088+leilii@users.noreply.github.com>
2018-10-16 15:56:02 +0100

    Create 4-simba/bot.py
    Added code to help Simba roar with a user-defined function.

---
+++ 1-basics/5-functions/3-cinderella/4-simba/bot.py
@@ -0,0 +1,13 @@
+def roar(num_roars):
+     for n in range(0,num_roars,1):
+       print("roar!")
+     if (num_roars<3):
+         print("cough! cough!")
+     else:
+         print("ROAR!")
+       
+
+
+# The following are calls to the function for testing purposes
+roar(1)
+roar(3)
