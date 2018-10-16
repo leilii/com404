337863d19e8ff4fb25e72aa48d5a1c1f45890117
leilii <43784088+leilii@users.noreply.github.com>
2018-10-16 15:28:17 +0100

    Create 2-leisure/bot.py
    Added code to select a leisure activity with a user-defined function.

---
+++ 1-basics/5-functions/1-greeting/2-leisure/bot.py
@@ -0,0 +1,11 @@
+def activity():
+   print("Please enter your acrtivity you like: ")
+   act=str(input())
+   if (act=="watch movie"): 
+      print("watching a movie sounds like fun")
+   elif (act=="play"): 
+      print("I have lots of toys to play with.") 
+   else:  
+      print("I am not sure if I will like it but I will give it a try.") 
+
+activity() 
