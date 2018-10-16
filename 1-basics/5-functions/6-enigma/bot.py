de5f2197ad0963a590f5195cddfc4dc8ca8a635f
leilii <43784088+leilii@users.noreply.github.com>
2018-10-16 16:48:50 +0100

    Create 6-enigma/bot.py
    Added code to generate encoded strings with a user-defined function.

---
+++ 1-basics/5-functions/6-enigma/bot.py
@@ -0,0 +1,18 @@
+def run():
+  print("Please enter a character to display...")
+  cha=input()
+  print("Please enter total number of characters...")
+  num1=int(input())
+  print("Please enter a whole number ...")
+  num2=int(input())
+
+  for i in range(0,num1,1):
+     m=num2*i
+     if (i+1%num2==0):
+      print(cha,end="")
+     else:
+       print("-",end="") 
+	
+
+
+run()
